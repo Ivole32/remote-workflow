c247fcb5df12860fc633cd54658b2284abc76db1
Ivole32 <167101401+Ivole32@users.noreply.github.com>
2025-06-24 21:52:47 +0200

    Create workflow_handler.py

---
+++ workflow_handler.py
@@ -0,0 +1,15 @@
+from logger import Logger
+from config import Configuration
+
+class workflow_handler:
+    def __init__(self) -> None:
+        self.logger = Logger()
+        self.config = Configuration()
+
+    def create_workflow(self) -> None:
+        """
+        A function to create workflows
+        """
+        pass
+
+# Nothing is implemented.
